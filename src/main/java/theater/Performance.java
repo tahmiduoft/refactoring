458a121c046c44ec5d713e5938d3446fda9c0ff8
tahmiduoft <tahmid.zaman@mail.utoronto.ca>
2025-11-19 01:35:35 -0500

    Task 1.1: Fix CheckStyle issues

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,36 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playId;
+    private final int audience;
 
-    public Performance(String playID, int audience) {
-        this.playID = playID;
+    /**
+     * Creates a performance for a play.
+     * @param playId the identifier of the play being performed
+     * @param audience the audience size for the performance
+     */
+    public Performance(String playId, int audience) {
+        this.playId = playId;
         this.audience = audience;
+    }
+
+    /**
+     * Returns the play identifier for this performance.
+     * @return the play identifier
+     */
+    public String getPlayId() {
+        return playId;
+    }
+
+    /**
+     * Returns the audience size for this performance.
+     * @return the audience size
+     */
+    public int getAudience() {
+        return audience;
     }
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,36 @@
 package theater;
 
+/**
+ * Class representing a play.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
+    /**
+     * Creates a play with a name and type.
+     * @param name the display name of the play
+     * @param type the play type used for billing
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Returns the name of the play.
+     * @return the play name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the type of the play.
+     * @return the play type
+     */
+    public String getType() {
+        return type;
     }
 }
